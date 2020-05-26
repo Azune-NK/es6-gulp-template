1a26bbc417337ddd04a7590491f4033a31dba7f9
Azune-NK <w1ndy@inbox.ru>
2020-05-26 15:44:03 +0300

    Добавляет разделение css на компоненты + фиксит мелочи

--- gulpfile.js
+++ gulpfile.js
@@ -127,6 +127,7 @@ gulp.task(`copy`, function () {
   return gulp.src([
     `source/fonts/**/*.{woff,woff2}`,
     `source/img/**`,
+    `source/video/**`,
     `source/favicon/**`,
   ], {
     base: `source`,
